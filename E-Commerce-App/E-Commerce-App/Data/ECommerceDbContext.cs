64d7ed9fa77a669d131a5dff3103f0ae75099f51
MutazTB <mutazaltbakhi900@gmail.com>
2022-05-30 23:22:40 +0300

    Add Data

--- E-Commerce-App/E-Commerce-App/Data/ECommerceDbContext.cs
+++ E-Commerce-App/E-Commerce-App/Data/ECommerceDbContext.cs
@@ -16,6 +16,25 @@ namespace E_Commerce_App.Data
         public ECommerceDbContext(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Clothes", Details = "Clothes" },
+                new Category { Id = 2 , Name = "Cars" , Details = "Cars"}
+
+              );
+
+            modelBuilder.Entity<Product>().HasData(
+               new Product { Id = 1, Name = "Jeans", Description = "Jeans" , ImageUrl = "Jeans.Url" , Price = 12 , CategoryId = 1},
+               new Product { Id = 2, Name = "Jeans", Description = "Jeans", ImageUrl = "Jeans.Url", Price = 12 , CategoryId = 1},
+               new Product { Id = 3, Name = "BMW", Description = "BMW", ImageUrl = "BMW.Url", Price = 12000, CategoryId = 2 }
+
+
+             );
+        }
     }
-}
+
+   
+    }
---
+++ E-Commerce-App/E-Commerce-App/Migrations/20220530202118_add_Data.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace E_Commerce_App.Migrations
+{
+    public partial class add_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Details", "Name" },
+                values: new object[] { 1, "Clothes", "Clothes" });
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Details", "Name" },
+                values: new object[] { 2, "Cars", "Cars" });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 1, 1, "Jeans", "Jeans.Url", "Jeans", 12.0 });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 2, 1, "Jeans", "Jeans.Url", "Jeans", 12.0 });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 3, 2, "BMW", "BMW.Url", "BMW", 12000.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
